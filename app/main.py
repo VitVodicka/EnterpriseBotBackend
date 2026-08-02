8e71fcdcb845ee0446c5e67d4df9baf69bd15c8d
Vit <xvodick6@mendelu.cz>
2026-08-02 13:55:25 +0200

    moved main to routes

--- app/main.py
+++ app/main.py
@@ -1,48 +1,7 @@
-from typing import Annotated, List
+from fastapi import FastAPI
 
-from fastapi import Depends, FastAPI, File, Form, UploadFile
-from tenacity import R
-from app.models.introduction_models.company_introduction_model import CompanyIntroductionModel
-from app.services.gemini.company_intent_extractor_service import CompanyIntentExtractorService
-from app.services.gemini.evaluate_service import EvaluateService
-from app.services.gemini.extract_service import ExtractService
-from app.services.gemini.recommendation_service import RecommendationService
-from app.models.extract_models.extracted_model import ExtractedModel
-from app.models.evaluate_models.evaluated_model import EvaluatedModel
+from app.api.v1.routes import upload
 
-from app.deps import (
-    get_intent_extractor_service,
-    get_extract_service,
-    get_evaluate_service,
-    get_recommendation_service,
-)
+app = FastAPI(title="CV Recommender")
 
-app = FastAPI()
-
-@app.post("/upload-cvs")
-#zapracovat na tom companyIntroduction: CompanyIntroductionModel,
-async def upload(company_introduction: Annotated[str, Form()], files: List[UploadFile] = File(...), job_ad: str = Form(""),
-    intent_service: CompanyIntentExtractorService = Depends(get_intent_extractor_service),
-    extract_service: ExtractService = Depends(get_extract_service),
-    evaluate_service: EvaluateService = Depends(get_evaluate_service),
-    recommendation_service: RecommendationService = Depends(get_recommendation_service)):
-    
-    results = []
-    
-    for f in files:
-        results.append({"filename": f.filename, "error": "Není PDF"})
-        continue
-    
-    company_data = CompanyIntroductionModel.model_validate_json(company_introduction)
-
-
-    print("počet:" + str(len(files)))
-    job_info = await intent_service.extract_company_intent(job_ad, company_data)
-
-
-    extracted_cvs: tuple[ExtractedModel, ExtractedModel] = await extract_service.extract_cv_data(files=files)
-
-    evaluated_cvs: tuple[EvaluatedModel, EvaluatedModel] = await evaluate_service.evaluate_cvs(extracted_cvs, job_info=job_info)
-    recommendation = await recommendation_service.recommend(job_info=job_info, evaluated_cvs=evaluated_cvs)
-
-    return recommendation
+app.include_router(upload.router)
